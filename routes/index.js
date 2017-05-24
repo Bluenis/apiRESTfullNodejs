ca4330c32052add15c4c6cdcd978ac0ba05cec04
mef <fondovila@gmail.com>
2017-05-24 05:25:00 -0300

    autentication

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express')
+const productCtrl = require('../controller/product')
+const userCtrl = require('../controller/user')
+const auth = require('../middlewares/auth')
+const api = express.Router()
+
+api.get('/product', productCtrl.getProducts)
+api.get('/product/:productId', productCtrl.getProduct)
+api.post('/product', auth, productCtrl.saveProduct)
+api.put('/product/:productId', auth, productCtrl.updateProduct)
+api.delete('/product/:productId', auth, productCtrl.deleteProduct)
+
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', auth, userCtrl.signIn)
+api.get('/private', auth, function(req,res){
+    res.status(200).send({message: 'Tienes acceso'})
+})
+module.exports = api
