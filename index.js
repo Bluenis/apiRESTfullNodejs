1af8184ee09da597b7e18d9615b3e8169943462c
mef <fondovila@gmail.com>
2017-05-26 03:39:46 -0300

    fix deprecation warning mpromise mongoose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
-
+mongoose.Promise = global.Promise;
 mongoose.connect(config.db,(err, res)=>{
     if (err) {
         return console.log(`Error en la base: ${err}`)
